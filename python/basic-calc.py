bbf790e849ab70f4182b37d41e2701c9de12894d
Juan <fuandavid9@gmail.com>
2025-11-02 13:03:42 -0500

    Python script for a basic calculator

---
+++ python/basic-calc.py
@@ -0,0 +1,14 @@
+
+#inputs
+num1 = 20
+num2 = 45
+#2. processes
+add = num1 + num2
+subs = num1 - num2
+mult = num1 * num2
+div = num1 / num2 
+#3. outputs
+print("addition: ", add,type(add))
+print("subtraction: ", subs,type(subs))
+print("multiplicacion: ", mult,type (mult))
+print("divison: ", div,type(div))
